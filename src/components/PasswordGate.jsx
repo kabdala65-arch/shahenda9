e5bba6e1c946e33ade0ec4b7e358166271829507
kabdala65-arch <kabdala65@gmail.com>
2026-09-12 23:39:26 +0300

    first commit

--- src/components/PasswordGate.jsx
+++ src/components/PasswordGate.jsx
@@ -3,7 +3,7 @@ import { motion, AnimatePresence } from 'framer-motion'
 import { useImageFallback } from '../hooks/useImageFallback'
 import './PasswordGate.css'
 
-const CORRECT_PASSWORD = '123'
+const CORRECT_PASSWORD = 'ام خلود'
 
 export default function PasswordGate({ onUnlock }) {
   const [value, setValue] = useState('')
